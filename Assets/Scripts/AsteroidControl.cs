ba22d5aae1563aca98d8535a41157a7d37cdc74c
Ken Vu <nghiavdng108@outlook.com>
2021-06-26 22:44:38 +0700

    add asteroid & edit enemy prefab

---
+++ Assets/Scripts/AsteroidControl.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidControl : MonoBehaviour
+{
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = transform.position;
+        position = new Vector2(position.x, position.y - speed*Time.deltaTime);
+        transform.position = position;
+
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+        if (min.y > transform.position.y) {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemy;
+    public GameObject asteroid;
 
-    float maxSpawnRate = 5f; //seconds
+    float maxEnemySpawnRate = 5f; //seconds
+    float maxAsteroidSpawnRate = 8f; //seconds
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("spawnEnemy", maxSpawnRate);
-
+        Invoke("spawnEnemy", maxEnemySpawnRate);
+        Invoke("spawnAsteroid", maxAsteroidSpawnRate);
         InvokeRepeating("IncreaseSpawnRate", 0f, 30f);
     }
 
@@ -29,26 +31,52 @@ public class EnemySpawner : MonoBehaviour
         GameObject anEnemy = (GameObject)Instantiate(enemy);
         anEnemy.transform.position = new Vector2(Random.Range(min.x, max.x), max.y);
 
-        ScheduleNextEnemySpawn();
+        ScheduleNextEnemySpawn("enemy");
     }
 
-    void ScheduleNextEnemySpawn() {
-        float spawnInSenconds;
-        if (maxSpawnRate > 1f) {
-            //pick a number between 1 and maxSpawnRate
-            spawnInSenconds = Random.Range(1f, maxSpawnRate);
-        }
-        else {
-            spawnInSenconds = 1f;
-        }
+    void spawnAsteroid() {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
 
-        Invoke("spawnEnemy", spawnInSenconds);
+        //spawn enemy
+        GameObject anAsteroid = (GameObject)Instantiate(asteroid);
+        anAsteroid.transform.position = new Vector2(Random.Range(min.x, max.x), max.y);
+
+        ScheduleNextEnemySpawn("asteroid");
+    }
+
+    void ScheduleNextEnemySpawn(string enemyName) {
+        float spawnInSenconds;
+        if (enemyName == "enemy") {
+            
+            if (maxEnemySpawnRate > 1f) {
+                //pick a number between 1 and maxSpawnRate
+                spawnInSenconds = Random.Range(1f, maxEnemySpawnRate);
+            }
+            else {
+                spawnInSenconds = 1f;
+            }
+            Invoke("spawnEnemy", spawnInSenconds);
+        }
+        else if (enemyName == "asteroid") {
+            if (maxAsteroidSpawnRate > 1f) {
+                //pick a number between 1 and maxSpawnRate
+                spawnInSenconds = Random.Range(4f, maxAsteroidSpawnRate);
+            }
+            else {
+                spawnInSenconds = 1f;
+            }
+            Invoke("spawnAsteroid", spawnInSenconds);
+        }
+        
+
+        
     }
 
     //Function to increase difficulty
     void IncreaseSpawnRate() {
-        if (maxSpawnRate > 1f) {
-            maxSpawnRate--;
+        if (maxEnemySpawnRate > 1f) {
+            maxEnemySpawnRate--;
         }
         else {
             CancelInvoke("IncreaseSpawnRate");
